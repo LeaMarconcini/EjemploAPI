5c11c8ba0305477980f03ab853bc15bbab88151f
LeaMarconcini <marconcinileandro2043@gmail.com>
2023-06-02 23:16:17 -0300

    Terminado el ejemplo

--- Ejemplo/Program.cs
+++ Ejemplo/Program.cs
@@ -1,3 +1,6 @@
+using ejemploApi.Data;
+using ejemploApi.Data.Repositorios;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var mySQLConfiguration = new MySQLConfiguration(builder.Configuration.GetConnectionString("MySqlConnection"));
+
+builder.Services.AddSingleton(mySQLConfiguration);
+
+builder.Services.AddScoped<ICarRepository, CarRepository>();
 
 var app = builder.Build();
 
--- ejemploApi.Data/Repositorios/CarRepository.cs
+++ ejemploApi.Data/Repositorios/CarRepository.cs
@@ -1,37 +1,80 @@
-﻿using ejemploApi.Model;
+﻿using Dapper;
+using ejemploApi.Model;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ejemploApi.Data.Repositorios
 {
-    internal class CarRepository : ICarRepository
+    public class CarRepository : ICarRepository
     {
-        public Task<bool> DeleteCar(Car car)
+        private readonly MySQLConfiguration _connectionString;
+        public CarRepository(MySQLConfiguration connectionString) 
         {
-            throw new NotImplementedException();
+            _connectionString = connectionString;
         }
 
-        public Task<IEnumerable<Car>> GetAllCars()
+        protected MySqlConnection dbConnection()
         {
-            throw new NotImplementedException();
+            return new MySqlConnection(_connectionString.ConnectionString);
         }
 
-        public Task<Car> GetDetails(int id)
+        public async Task<bool> DeleteCar(Car car)
         {
-            throw new NotImplementedException();
+            var db = dbConnection();
+            var sql = @"DELETE FROM cars WHERE id= @id";
+            var result = await db.ExecuteAsync(sql, new { id = car.id });
+            return result > 0;
         }
 
-        public Task<bool> InsertCar(Car car)
+        public async Task<IEnumerable<Car>> GetAllCars()
         {
-            throw new NotImplementedException();
+            var db = dbConnection();
+            var sql = @"SELECT id, make, model, color, years, doors FROM cars";
+
+            return await db.QueryAsync<Car>(sql, new { });
         }
 
-        public Task<bool> UpdateCar(Car car)
+        public async Task<Car> GetDetails(int id)
         {
-            throw new NotImplementedException();
+            var db = dbConnection();
+            var sql = @"SELECT id, make, model, color, years, doors
+                        FROM cars 
+                        WHERE id = @id";
+
+            return await db.QueryFirstOrDefaultAsync<Car>(sql, new { id = id });
+        }
+
+        public async Task<bool> InsertCar(Car car)
+        {
+            var db = dbConnection();
+            var sql = @"INSERT INTO cars (make, model, color, years, doors)
+                        values (@make, @model, @color, @years, @doors";
+
+            var result = await db.ExecuteAsync(sql, new 
+            { car.make, car.model, car.color, car.years, car.doors });
+            return result > 0;
+        }   
+
+        public async Task<bool> UpdateCar(Car car)
+        {
+            var db = dbConnection();
+            var sql = @"UPDATE cars
+                        SET make=@make,
+                        model=@model,
+                        color=@color,
+                        years=@years,
+                        doors=@doors
+                        WHERE id = @id
+                        ";
+
+            var result = await db.ExecuteAsync(sql, new
+            { car.make, car.model, car.color, car.years, car.doors, car.id});
+            return result > 0;
         }
     }
 }
